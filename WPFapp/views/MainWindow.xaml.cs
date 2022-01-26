68e1c0b7ca5fc343a5bea89e2a6e4d68c3b43d47
goluch <goluch@eti.pg.edu.pl>
2022-01-26 22:39:22 +0100

    moved MainWindow.xaml to views directory

--- WPFapp/views/MainWindow.xaml.cs
+++ WPFapp/views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFapp
+namespace WPFapp.views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
